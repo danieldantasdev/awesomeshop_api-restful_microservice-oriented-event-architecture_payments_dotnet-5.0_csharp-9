2a1fca6cd6d3e1e239955b015a36099dddf8d9c4
danieldantasdev <contatolobinhodev@gmail.com>
2023-07-07 10:20:45 -0300

    chore: move directory

--- AwesomeShop.Services.Payments-master/AwesomeShop.Services.Payments.Api/Infrastructure/Repositories/MongoDbOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace AwesomeShop.Services.Payments.Api.Infrastructure.Repositories
-{
-    public class MongoDbOptions
-    {
-        public string ConnectionString { get; set; }
-        public string Database { get; set; }
-    }
-}
